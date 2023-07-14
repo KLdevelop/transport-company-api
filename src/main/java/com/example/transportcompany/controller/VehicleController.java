cebca5966a8eabe62edb3104e60d124a2b938116
KLdevelop <drapdrop1@gmail.com>
2023-07-14 12:26:48 +0300

    filter method

---
+++ src/main/java/com/example/transportcompany/controller/VehicleController.java
@@ -0,0 +1,32 @@
+package com.example.transportcompany.controller;
+
+import com.example.transportcompany.dto.VehicleDto;
+import com.example.transportcompany.dto.VehicleFilterDto;
+import com.example.transportcompany.model.Vehicle;
+import com.example.transportcompany.service.VehicleService;
+import com.example.transportcompany.util.MappingUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class VehicleController {
+    @Autowired
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    private final VehicleService vehicleService;
+
+    @PostMapping("/filter")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    List<VehicleDto> filterVehicles(@RequestBody VehicleFilterDto filter) {
+        final List<Vehicle> vehicles = vehicleService.filter(filter);
+
+        return vehicles.stream().map(MappingUtils::mapToVehicleDto).toList();
+    }
+}
